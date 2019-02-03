183e81ab8b75bbc56f4a2f6f0e043fbbc96c1b5f
stephenb <stephen.bartholomew@paxport.net>
2019-02-03 16:13:12 +0000

    Remove passphrase for SSH key

--- src/test/java/tech/verenti/support/PayLoad.java
+++ src/test/java/tech/verenti/support/PayLoad.java
@@ -7,12 +7,13 @@ public class PayLoad {
     public PayLoad() {
     }
 
-    private static String[] Possessive = {"my", "her", "his"};
+    private static String[] Possessive = {"my", "her", "his", "Des Lynam's", "Bernard Cribbins'", "Brian Cant's"};
 
     private static String[] MainClause = {"father", "mother", "brother", "sister"};
 
     private static String[] AdverbialClause = {"cooked a nice meal of dumplings and custard", "rode to the post office", "rode the winner at the 14:50 at Thirsk",
-            "became agitated when their football was stolen "};
+            "rode Apollo Avenger at the 13:20 at Chepstow", "became agitated when their football was stolen", "decided not to drown the kittens after all", "ate their meal with indecent haste", "fought Mohammed Ali and won",
+    "is a world leading expert on firearms", "invented the self inflating air-bed", "scored the winning goal in the 1958 cup final", "replaced Jeff Pinkus on bass in Melvins", "invented the steam duvet", "says: You Can't Judge a Book By Its Cover" };
 
     private static Random rand = new Random();
 
